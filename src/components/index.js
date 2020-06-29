051a86318ea469a29885a046e1bc796f581952d5
kennyklee123 <kennyklee123@gmail.com>
2020-06-28 21:30:02 -0700

    began working on the API calls

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+//needed for the import statement in App.js
+export { default as Cards} from './Cards/Cards';
+export { default as Chart} from './Chart/Chart';
+export { default as CountryPicker} from './CountryPicker/CountryPicker';
+
