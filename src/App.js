cb51c0b7a0431a6b463942db279c8389c5dc3aaf
kennyklee123 <kennyklee123@gmail.com>
2020-08-04 11:06:48 -0700

    Added more info about the project and a link to the code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 import ronaImage from './images/image.png';
-
 //when we have index files like ./api or components we don't need to specify it
 
 /*
@@ -42,6 +41,8 @@ class App extends React.Component {
         return (
             <div className = {styles.container}>
                 <img className = {styles.image} alt = "rona" src = {ronaImage} />
+                <h2>I created this project during the Summer of Shipping using React, Material-UI, and Chart.js!</h2>
+                <h2>All the information found here is fetched from John Hopkins University! Check out the code <a id = {styles.github} href="https://github.com/kennyklee123/The-Virus-Tracker">here</a></h2>
                 <Cards data = {data} />
                 <CountryPicker handleCountryChange = {this.handleCountryChange}/>
                 <Chart data = {data} country = {country}/>
